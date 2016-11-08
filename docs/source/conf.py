adf0052e5e4b6e0cec943ebdf07024a99d1994f0
Song Jin <song@Songs-MacBook-Pro.local>
2016-11-08 23:25:08 +1100

    improve on doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
 ]
+import sphinx_rtd_theme
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['statictemplates']
@@ -123,7 +124,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
